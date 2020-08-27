e57efa0d68cdd7093744562bb9afd71911a4e115
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-27 12:32:54 -0400

    express

--- app/app.js
+++ app/app.js
@@ -1,31 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
-const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
+// var port = 5000;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds117625.mlab.com:17625/farsi');
+
+var index = require('./routes/index');
+var farsiRouter = require('./routes/farsi');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Change the HTTP 'method' of the request to handle PUT and DELETE requests if they are sent in the _method property of a form
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
 app.use(cookieParser());
-app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
-const mainRoutes = require('./routes');
-const cardRoutes = require('./routes/cards');
+app.use('/', index);
+app.use('/farsis', farsiRouter);
 
-app.use(mainRoutes);
-app.use('/cards', cardRoutes);
-
-app.use((req, res, next) => {
-  const err = new Error("ERROR, BITCHES!");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 
-app.use((err,req,res,next) => {
-res.locals.error = err;
-res.status(err.status);
-res.render('error');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log("The app is running on Port 3000!");
-});
+module.exports = app;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,33 +1,9 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-router.get('/', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-      res.render('index', {name: name});
-  } else {
-    res.redirect('/hello');
-  }
-});
-
-
-router.get('/hello', (req, res) => {
-  const name = req.cookies.username;
-    if (name) {
-      res.redirect('/');
-  } else {
-    res.render('hello');
-  }
-});
-
-router.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-router.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/hello');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
